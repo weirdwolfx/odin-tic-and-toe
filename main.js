acc629d05128e64924fe370287248e84c691b697
weirdwolfx <aadityapgoyal@gmail.com>
2025-10-16 00:42:53 +0530

    Add Game and ButtonEvent class
    Make code more organized and lay strong foundations before scaling

--- main.js
+++ main.js
@@ -40,3 +40,88 @@ const ScreenManager = (function(){
     }
 })()
 
+
+// make the different modes available more scalable 
+const MODES = {
+    COMPUTER: 'computer',
+    HUMAN: 'human',
+}
+
+
+class Game {
+    constructor() {
+        ScreenManager.initializeScreens({
+            'start': 'start-game',
+            'mode': 'choose-mode',
+            'play': 'play-game',
+            'level': 'choose-difficulty',
+        });
+        // mode : whom are you going to play against? computer or player
+        this._mode = null;
+        // difficulty level
+        this._level = null;
+    }
+
+    // open the game
+    start() {
+        ScreenManager.showScreen('start');
+    }
+
+    // open the screen where player has to select mode
+    selectMode() {
+        ScreenManager.showScreen('mode');
+    }
+
+    // open screen where player has to choose difficulty
+    selectDifficulty() {
+        ScreenManager.showScreen('level');
+    }
+
+    // play game given mode = mode, and level
+    play(mode, level = null) {
+        // not a valid mode
+        if (mode != MODES.COMPUTER && mode != MODES.HUMAN) {
+            console.log(`Invalid mode: ${mode} does not exist`);
+            return;
+        }
+        if (mode == MODES.COMPUTER) {
+            // level must be between 1(easy) and 3(hard)
+            if (level == null || level < 1 || level > 3) {
+                console.log(`Invalid level`);
+                return;
+            }
+            this._level = level;
+        }
+        this._mode = mode;
+        // open screen to play game
+        ScreenManager.showScreen('play');
+    }   
+}
+
+
+// handle all button clicks
+const ButtonEventHandler = (function(){
+    function initializeButtons(game) {
+        addBtnListener('.start-btn', () => game.selectMode());
+        addBtnListener('.computer-btn', () => game.selectDifficulty());
+        addBtnListener('.human-btn', () => game.play(MODES.HUMAN));
+        addBtnListener('.back-btn', () => game.start());
+        document.querySelectorAll('.level-btn').forEach((btn) => {
+            btn.addEventListener('click', () => game.play(MODES.COMPUTER, parseInt(btn.id)));
+        })
+    }
+
+    // add eventListener to button.className with event function as func
+    function addBtnListener(className, func) {
+        document.querySelector(className).addEventListener('click', func);
+    }
+
+    return {
+        initializeButtons,
+    }
+})()
+
+
+const game = new Game()
+game.start();
+ButtonEventHandler.initializeButtons(game);
